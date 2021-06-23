a4bcfdc54253772aa12b38cb5098bf98a29150e8
Islam-Miko <islamimanhod@gmail.com>
2021-06-23 14:10:56 +0600

    task2 is solved

--- app1/serializers.py
+++ app1/serializers.py
@@ -5,14 +5,33 @@ from .models import StoryFile, UserStoryInfo, Story
 class StoryFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = StoryFile
-        fields = '__all__'
+        fields = (
+            'id',
+            'detailedUrl',
+            'detailedText',
+            'content_type',
+            'content',
+            'duration',
+        )
 
+
+class StoryFileSerializer2(serializers.ModelSerializer):
+    class Meta:
+        model = StoryFile
+        fields = (
+            'id',
+            'detailedUrl',
+            'detailedText',
+            'content_type',
+            'content',
+            'duration',
+            'is_watched',
+        )
 
 class StorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Story
         fields = '__all__'
-
 
 class UserStoryInfoSer(serializers.ModelSerializer):
     class Meta:
