483d3a9c49e2d797ee17eca65dedede38d8621c0
Islam-Miko <islamimanhod@gmail.com>
2021-06-24 11:07:07 +0600

    SQLite to Postgres migration

--- SocialNet/settings.py
+++ SocialNet/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'SocialNet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'story',
+        'USER': 'miko',
+        'PASSWORD': 'python2021',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
